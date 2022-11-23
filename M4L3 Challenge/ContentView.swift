3845633ddc780ab22a4971d6daadeab0b6b2645c
David Morton <dpmorton@gmail.com>
2022-11-23 16:08:48 -0500

    Finished challenge

--- M4L3 Challenge/ContentView.swift
+++ M4L3 Challenge/ContentView.swift
@@ -8,18 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tabIndex = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView(selection: $tabIndex) {
+            
+            Text("This Tab’s tag is \(tabIndex)")
+                .tag(1)
+                .tabItem {
+                    VStack{
+                        Image(systemName:"tortoise.fill")
+                        Text("Tab 1")
+                    }
+                }
+            
+            Button("This is Tab 2.\nTake me to Tab 3", action: {
+                tabIndex = 3
+            })
+                .tag(2)
+                .tabItem {
+                    VStack{
+                        Image(systemName: "arrow.right")
+                        Text("Tab 2")
+                    }
+                }
+               
+            Text("This is Tab \(tabIndex)")
+                .tag(3)
+                .tabItem{
+                    VStack{
+                        Image(systemName: "hands.clap.fill")
+                        Text("Tab 3")
+                    }
+                }
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+            
+            
+    struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
